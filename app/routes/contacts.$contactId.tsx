45934b3d20fa8bd714330eb547aa8574d830d4f7
Keturah Howard <keturah.dev@gmail.com>
2025-04-20 14:56:40 -0700

    use data in contact component

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,17 +1,21 @@
-import { Form } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
+import { getContact } from "../data";
 
 import type { ContactRecord } from "../data";
+import { LoaderFunctionArgs } from "@remix-run/node";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const contact = await getContact(params.contactId!);
+  return {
+    contact,
+  };
+};
 
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placecats.com/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const { contact } = useLoaderData<typeof loader>();
+
+  if (!contact) return <>Oops! Something went wrong!</>;
 
   return (
     <div id="contact">
